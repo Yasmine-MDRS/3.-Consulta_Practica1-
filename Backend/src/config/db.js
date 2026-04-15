f934a5136c52d5acc65142ccf8997fbbb5e75c13
Yasmine-MDRS <a22300947@ceti.mx>
2026-04-15 08:33:02 -0600

    Práctica 1 Eccomerce

---
+++ Backend/src/config/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const conexion= mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+conexion.connect((err)=>{
+  if(err){
+    console.error('Error al conectar con MySQL: ', err);
+    return;
+  }
+    console.log('Conexión exitosa a MySQL');
+}); module.exports = conexion;  
