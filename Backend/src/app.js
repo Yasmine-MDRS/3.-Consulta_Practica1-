86e098784c8924d99c7309478138c32954437bb5
Yasmine-MDRS <a22300947@ceti.mx>
2026-05-15 20:20:08 -0600

    Login, Register y Catalogo corregido

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const productRoutes = require('./routes/productos.routes.js');
 const paypalRoutes = require('./routes/paypal.routes.js');
-
+const authRoutes = require('./routes/auth.routes.js');
 const app = express();
 
 app.use(cors());
@@ -11,5 +11,5 @@ app.use(express.json());
 
 app.use('/api/productos', productRoutes);
 app.use('/api/paypal', paypalRoutes);
-
+app.use('/api/auth', authRoutes);
 module.exports = app;
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,24 @@ import { Routes } from '@angular/router';
 import {Catalogo} from './components/catalogo/catalogo';
 import { CarritoComponent } from './components/carrito/carrito';
 import { CheckoutComponent } from './components/checkout/checkout';
-import{ Busqueda } from './components/busqueda/busqueda';
+import{ BusquedaComponent } from './components/busqueda/busqueda';
 import { ProductDetalle } from './components/producto-detalle/product-detalle/product-detalle';
 import { CategoriasComponent } from './components/categorias/categorias';
+import { LoginComponent } from './components/login/login';
+import { RegisterComponent } from './components/register/register';
 //import { PerfilComponent } from './components/perfil/perfil';
 import{Home} from './components/home/home';
 
 export const routes: Routes = [
-  { path: "", component: Home },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+ {path:'home',component:Home},
   { path: 'catalogo', component: Catalogo }, 
   { path: 'carrito', component: CarritoComponent },
   { path: 'checkout', component: CheckoutComponent },
-  {path:'buscar',component:Busqueda},
+  {path:'buscar',component:BusquedaComponent},
   { path: 'producto/:id', component: ProductDetalle},
   { path: 'categorias', component: CategoriasComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: "" } 
 ];
